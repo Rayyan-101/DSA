1db03384c6b748e870e5eeea7302c2f794e997df
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 18:51:52 +0530

    New commit

--- graph14.cpp
+++ graph14.cpp
@@ -1,10 +1,16 @@
-int count = 0;
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int count = 0;
+
     void dfs(int node, int parent, vector<vector<pair<int, int>>>& adj, vector<bool>& visited) {
         visited[node] = true;
         
         for (auto& [child, sign] : adj[node]) {
             if (!visited[child]) {
-                count += sign;
+                count += sign;  // if sign == 1, this edge needs to be reversed
                 dfs(child, node, adj, visited);
             }
         }
@@ -12,11 +18,36 @@ int count = 0;
 
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<pair<int, int>>> adj(n);
+        
+        // sign = 1 means original direction is from u -> v (might need to reverse)
+        // sign = 0 means v -> u (already towards 0 direction if we go correctly)
         for (auto& connection : connections) {
-            adj[connection[0]].push_back({connection[1], 1});
-            adj[connection[1]].push_back({connection[0], 0});
+            int u = connection[0];
+            int v = connection[1];
+            adj[u].push_back({v, 1}); // original direction u -> v
+            adj[v].push_back({u, 0}); // reverse edge v -> u with sign 0
         }
+        
         vector<bool> visited(n, false);
         dfs(0, -1, adj, visited);
         return count;
-        
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int n = 6;
+    vector<vector<int>> connections = {
+        {0,1},
+        {1,3},
+        {2,3},
+        {4,0},
+        {4,5}
+    };
+
+    int ans = sol.minReorder(n, connections);
+    cout << "Minimum roads to reorder: " << ans << endl;  // Expected: 3
+
+    return 0;
+}
