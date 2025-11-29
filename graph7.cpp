72b65b3755a5e4a5534df374c08f3a552a3708e8
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 16:34:01 +0530

    New commit

--- graph7.cpp
+++ graph7.cpp
@@ -1,11 +1,17 @@
-int target;
-    void dfs(vector<vector<int>>& adj, int start, vector<int>& temp, vector<vector<int>>& result ) {
-        if(start == target) {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int target;
+
+    void dfs(vector<vector<int>>& adj, int start, vector<int>& temp, vector<vector<int>>& result) {
+        if (start == target) {
             result.push_back(temp);
             return;
         }
         
-        for(int &x : adj[start]) {
+        for (int x : adj[start]) {
             temp.push_back(x);
             dfs(adj, x, temp, result);
             temp.pop_back();
@@ -15,8 +21,39 @@ int target;
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> result;
         vector<int> temp;
-        target = graph.size()-1;
-        temp.push_back(0);
+
+        target = graph.size() - 1;
+        temp.push_back(0);  // start from node 0
+        
         dfs(graph, 0, temp, result);
         
         return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> graph = {
+        {1, 2},
+        {3},
+        {3},
+        {}
+    };
+
+    vector<vector<int>> paths = sol.allPathsSourceTarget(graph);
+
+    cout << "All paths from source to target:\n";
+    for (auto &path : paths) {
+        for (int node : path) {
+            cout << node << " ";
+        }
+        cout << "\n";
+    }
+
+    // Expected Output:
+    // 0 1 3
+    // 0 2 3
+
+    return 0;
+}
