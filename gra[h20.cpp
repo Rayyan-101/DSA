92a92dd60db732d160ebd8ff795d75d8c997027a
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 19:05:40 +0530

    New commit

--- gra[h20.cpp
+++ gra[h20.cpp
@@ -1,100 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DSU {
 private:
-        vector<int> parent;
-        vector<int> rank;
-        int components;
+    vector<int> parent;
+    vector<int> rankv;   // renamed from rank to avoid name clashes
+    int components;
 
-    public : 
-        
-        DSU(int n) {
-            parent.resize(n+1);
-            rank.resize(n+1);
-            components = n;
-            for(int i = 1; i<=n; i++) {
-                parent[i] = i;
-            }
+public: 
+    DSU(int n) {
+        parent.resize(n + 1);
+        rankv.assign(n + 1, 0);
+        components = n;
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
         }
-    
-        int find (int x) {
-            if (x == parent[x]) 
-                return x;
+    }
 
-            return parent[x] = find(parent[x]);
+    int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]); // path compression
+    }
+
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent)
+            return;
+
+        if (rankv[x_parent] > rankv[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if (rankv[x_parent] < rankv[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rankv[y_parent]++;
         }
+        components--;
+    }
 
-        void Union (int x, int y) {
-            int x_parent = find(x);
-            int y_parent = find(y);
-
-            if (x_parent == y_parent) 
-                return;
-
-            if(rank[x_parent] > rank[y_parent]) {
-                parent[y_parent] = x_parent;
-            } else if(rank[x_parent] < rank[y_parent]) {
-                parent[x_parent] = y_parent;
-            } else {
-                parent[x_parent] = y_parent;
-                rank[y_parent]++;
-            }
-            components--;
-        }
-    
-        bool isSingle() {
-            return components == 1;
-        }
+    bool isSingle() {
+        return components == 1;
+    }
 };
 
 class Solution {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        
+        // Sort by type descending so type 3 (shared) edges come first
         auto lambda = [&](vector<int>& v1, vector<int>& v2) {
             return v1[0] > v2[0];
         };
         
-        sort(begin(edges), end(edges), lambda);
+        sort(edges.begin(), edges.end(), lambda);
+        
         DSU Alice(n);
         DSU Bob(n);
         
         int addedEdge = 0;
         
-        for (auto &edge: edges) { // For each edge -
+        for (auto &edge: edges) {
             int type = edge[0];
             int u = edge[1];
             int v = edge[2];
             
-            if(type == 3) {
+            if (type == 3) {
                 bool add = false;
                 
-                if(Alice.find(u) != Alice.find(v)) {
+                if (Alice.find(u) != Alice.find(v)) {
                     Alice.Union(u, v);
                     add = true;
                 }
                 
-                if(Bob.find(u) != Bob.find(v)) {
+                if (Bob.find(u) != Bob.find(v)) {
                     Bob.Union(u, v);
                     add = true;
                 }
                 
-                if(add)
+                if (add)
                     addedEdge++;
                 
-            } else if(type == 2) {
-                if(Bob.find(u) != Bob.find(v)) {
+            } else if (type == 2) { // Bob only
+                if (Bob.find(u) != Bob.find(v)) {
                     Bob.Union(u, v);
                     addedEdge++;
                 }
-            } else {
-                if(Alice.find(u) != Alice.find(v)) {
+            } else { // type == 1, Alice only
+                if (Alice.find(u) != Alice.find(v)) {
                     Alice.Union(u, v);
                     addedEdge++;
                 }
             }
         }
         
-        
-        if(Alice.isSingle() && Bob.isSingle()) {
-            return edges.size() - addedEdge;
+        if (Alice.isSingle() && Bob.isSingle()) {
+            return (int)edges.size() - addedEdge;
         }
         
         return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int n = 4;
+    vector<vector<int>> edges = {
+        {3,1,2},
+        {3,2,3},
+        {1,1,3},
+        {1,2,4},
+        {1,1,2},
+        {2,3,4}
+    };
+
+    int ans = sol.maxNumEdgesToRemove(n, edges);
+    cout << "Maximum number of removable edges: " << ans << endl; 
+    // Expected: 2
+
+    return 0;
+}
