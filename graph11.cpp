69dc0302f96100d0a9bd957c202255ace1bbe372
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 18:45:14 +0530

    New commit

--- graph11.cpp
+++ graph11.cpp
@@ -1,11 +1,16 @@
- int n;
-    
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int n;
+
     void dfs(vector<int>& edges, int startNode, vector<int>& dist_node, vector<bool>& visited) {
         visited[startNode] = true;
         
         int v = edges[startNode];
         
-        if(v != -1 && !visited[v]) {
+        if (v != -1 && !visited[v]) {
             visited[v] = true;
             dist_node[v] = 1 + dist_node[startNode];
             dfs(edges, v, dist_node, visited);
@@ -19,24 +24,42 @@
         vector<int> dist2(n, INT_MAX);
         vector<bool> visited1(n, false);
         vector<bool> visited2(n, false);
+        
         dist1[node1] = 0;
         dist2[node2] = 0;
         
         dfs(edges, node1, dist1, visited1);
         dfs(edges, node2, dist2, visited2);
         
-        
-        int minDistNode    = -1;
+        int minDistNode = -1;
         int minDistTillNow = INT_MAX;
+        
         for (int i = 0; i < n; i++) {
+            if (dist1[i] == INT_MAX || dist2[i] == INT_MAX)
+                continue;  // unreachable, skip
             
             int maxD = max(dist1[i], dist2[i]);
             
-            if (minDistTillNow > maxD) {
+            if (maxD < minDistTillNow) {
                 minDistNode = i;
                 minDistTillNow = maxD;
             }
         }
 
         return minDistNode;
-        
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> edges = {2,2,3,-1};
+    int node1 = 0;
+    int node2 = 1;
+
+    int ans = sol.closestMeetingNode(edges, node1, node2);
+    cout << "Closest meeting node: " << ans << endl;  
+    // Expected output: 2
+
+    return 0;
+}
