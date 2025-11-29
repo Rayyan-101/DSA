6c6fe11488edd2f6d1241292d1ec383b8daede04
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 19:15:04 +0530

    New commit

--- graph23.cpp
+++ graph23.cpp
@@ -1,14 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 typedef long long LL;
-    void DFS(int u, unordered_set<int> & visited, unordered_map<int, vector<int>> &adj) {
-        
+
+class Solution {
+public:
+    void DFS(int u, unordered_set<int> &visited, unordered_map<int, vector<int>> &adj) {
         visited.insert(u);
         
-        for(int &v : adj[u]) {
-            if(visited.find(v) == visited.end()) { //Not visited
+        for (int v : adj[u]) {
+            if (visited.find(v) == visited.end()) { // Not visited
                 DFS(v, visited, adj);
             }
         }
-
     }
     
     int maximumDetonation(vector<vector<int>>& bombs) {
@@ -16,10 +20,11 @@ typedef long long LL;
         
         unordered_map<int, vector<int>> adj;
         
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<n; j++) {
+        // Build directed graph: edge i -> j if bomb i can detonate bomb j
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 
-                if(i == j) //same bomb
+                if (i == j) // same bomb
                     continue;
                 
                 LL x1 = bombs[i][0];
@@ -28,24 +33,21 @@ typedef long long LL;
                 
                 LL x2 = bombs[j][0];
                 LL y2 = bombs[j][1];
-                LL r2 = bombs[j][2];
+                // LL r2 = bombs[j][2]; // not needed
                 
-                //Make a directed edge from i to j if i can detonate j as well
+                LL distance = (x2 - x1) * (x2 - x1) + 
+                              (y2 - y1) * (y2 - y1);
                 
-                LL distance = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
-                
-                if(LL(r1*r1) >= distance) {
+                if (r1 * r1 >= distance) {
                     adj[i].push_back(j);
                 }
-                
             }
         }
-        
         
         int result = 0;
         unordered_set<int> visited;
         
-        for(int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             DFS(i, visited, adj);
             int count = visited.size();
             result = max(result, count);
@@ -53,4 +55,19 @@ typedef long long LL;
         }
         
         return result;
-        
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> bombs = {
+        {2, 1, 3},
+        {6, 1, 4}
+    };
+
+    cout << "Maximum bombs detonated: "
+         << sol.maximumDetonation(bombs) << endl;  // Expected: 2
+
+    return 0;
+}
