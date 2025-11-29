3042b58cbfb0a321c3b6afe60bb22eeb76d51f35
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 18:42:26 +0530

    New commit

--- graph10.cpp
+++ graph10.cpp
@@ -1,12 +1,17 @@
-char DFS(unordered_map<char, vector<char>> &adj, char curr, vector<int>& visited) {
-        visited[curr-'a'] = 1;
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char DFS(unordered_map<char, vector<char>> &adj, char curr, vector<int>& visited) {
+        visited[curr - 'a'] = 1;
         
         char minChar = curr;
         
-        for(char &v : adj[curr]) {
-            
-            if(visited[v-'a'] == 0)
+        for (char &v : adj[curr]) {
+            if (visited[v - 'a'] == 0) {
                 minChar = min(minChar, DFS(adj, v, visited));
+            }
         }
         
         return minChar;
@@ -16,24 +21,37 @@ char DFS(unordered_map<char, vector<char>> &adj, char curr, vector<int>& visited
         int n = s1.length();
         unordered_map<char, vector<char>> adj;
         
-        for(int i = 0; i<n; i++) {
+        // Build graph of equivalences
+        for (int i = 0; i < n; i++) {
             char u = s1[i];
             char v = s2[i];
-            
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         
-        
         int m = baseStr.length();
         string result;
         
-        for(int i = 0; i<m; i++) {
+        for (int i = 0; i < m; i++) {
             char ch = baseStr[i];
-            
             vector<int> visited(26, 0);
-            
             result.push_back(DFS(adj, ch, visited));
         }
         
         return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    string s1 = "parker";
+    string s2 = "morris";
+    string baseStr = "parser";
+
+    string ans = sol.smallestEquivalentString(s1, s2, baseStr);
+    cout << "Smallest equivalent string: " << ans << endl;
+    // Expected output: "makkek"
+
+    return 0;
+}
