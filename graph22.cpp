4c91dec25b6c7e8880731ad23057837e3af983f6
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 19:12:10 +0530

    New commit

--- graph22.cpp
+++ graph22.cpp
@@ -1,58 +1,93 @@
- void dfs(unordered_map<string, vector<pair<string, double>>> &adj, string src, string dst, unordered_set<string>& visited, double product, double &ans) {
-        if(visited.find(src) != visited.end())
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(unordered_map<string, vector<pair<string, double>>> &adj,
+             const string &src, const string &dst,
+             unordered_set<string> &visited,
+             double product, double &ans) {
+        
+        if (visited.find(src) != visited.end())
             return;
         
         visited.insert(src);
-        if(src == dst) {
+        
+        if (src == dst) {
             ans = product;
             return;
         }
         
-        for(auto p : adj[src]) {
-            
+        for (auto &p : adj[src]) {
             string v   = p.first;
             double val = p.second;
-            
-            dfs(adj, v, dst, visited, product*val, ans);
-                
+            if (visited.find(v) == visited.end()) {
+                dfs(adj, v, dst, visited, product * val, ans);
+            }
         }
     }
     
-    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+    vector<double> calcEquation(vector<vector<string>>& equations,
+                                vector<double>& values,
+                                vector<vector<string>>& queries) {
         int n = equations.size();
         
         unordered_map<string, vector<pair<string, double>>> adj;
         
-        for(int i = 0; i<n; i++) {
-            
+        // Build graph: u/v = val  =>  u->v (val), v->u (1/val)
+        for (int i = 0; i < n; i++) {
             string u   = equations[i][0];
             string v   = equations[i][1];
             double val = values[i];
             
-            adj[u].push_back({v, val});        //To handle a/c
-            adj[v].push_back({u, 1.0/val});    //To handle c/a
+            adj[u].push_back({v, val});
+            adj[v].push_back({u, 1.0 / val});
         }
         
         vector<double> result;
         
-        for(auto &query : queries) {
-            
+        for (auto &query : queries) {
             string src = query[0];
             string dst = query[1];
             
             double ans     = -1.0;
             double product = 1.0;
             
-            
-            if(adj.find(src) != adj.end()) {
+            // Only search if src exists in graph
+            if (adj.find(src) != adj.end()) {
                 unordered_set<string> visited;
-                
                 dfs(adj, src, dst, visited, product, ans);
-                
             }
             
             result.push_back(ans);
-            
         }
         
         return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<string>> equations = {{"a","b"}, {"b","c"}};
+    vector<double> values = {2.0, 3.0};
+    vector<vector<string>> queries = {
+        {"a","c"},
+        {"b","a"},
+        {"a","e"},
+        {"a","a"},
+        {"x","x"}
+    };
+
+    vector<double> ans = sol.calcEquation(equations, values, queries);
+
+    cout.setf(ios::fixed);
+    cout << setprecision(5);
+    cout << "Results:\n";
+    for (double x : ans) {
+        cout << x << "\n";
+    }
+    // Expected (LeetCode example): [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+
+    return 0;
+}
