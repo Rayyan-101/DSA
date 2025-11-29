b26ba85abe05381318ff4e2477a59b2610f376e3
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 19:22:10 +0530

    New commit

--- graph25.cpp
+++ graph25.cpp
@@ -1,43 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 typedef pair<int, pair<int, int>> P;
-    vector<vector<int>> directions{{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
+
+class Solution {
+public:
+    vector<vector<int>> directions{
+        {1,1},{1,0},{0,1},{-1,-1},{-1,0},{0,-1},{1,-1},{-1,1}
+    };
+
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        if(m == 0 || n == 0 || grid[0][0] != 0)
+
+        if (m == 0 || n == 0 || grid[0][0] != 0)
             return -1;
 
         auto isSafe = [&](int x, int y) {
-            return x>=0 && x<m && y>=0 && y<n;
+            return x >= 0 && x < m && y >= 0 && y < n;
         };
-        
-        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
+
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
         priority_queue<P, vector<P>, greater<P>> pq;
-        
+
         pq.push({0, {0, 0}});
-        result[0][0] = 0;
-  
-        while(!pq.empty()) {
+        dist[0][0] = 0;
+
+        while (!pq.empty()) {
             int d  = pq.top().first;
-	    pair<int, int> node = pq.top().second;
-	    pq.pop();
+            int x  = pq.top().second.first;
+            int y  = pq.top().second.second;
+            pq.pop();
 
-            int x = node.first;
-            int y = node.second;
-            
-	    for(auto dir:directions) {
-		int x_   = x + dir[0];
-		int y_   = y + dir[1];
-		int dist = 1;
+            if (d > dist[x][y]) continue;
 
-		if(isSafe(x_, y_) && grid[x_][y_] == 0 && d+dist < result[x_][y_]) {
-		    pq.push({d+dist, {x_, y_}});
-		    grid[x_][y_] = 1;
-		    result[x_][y_] = d + dist;
-		}
+            for (auto &dir : directions) {
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
+
+                if (isSafe(x_, y_) && grid[x_][y_] == 0 && d + 1 < dist[x_][y_]) {
+                    dist[x_][y_] = d + 1;
+                    pq.push({dist[x_][y_], {x_, y_}});
+                }
             }
         }
-        
-        if(result[m-1][n-1] == INT_MAX)
-            return -1;
-        
-        return result[m-1][n-1]+1;
+
+        return dist[m-1][n-1] == INT_MAX ? -1 : dist[m-1][n-1] + 1;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> grid = {
+        {0,1},
+        {1,0}
+    };
+
+    cout << "Shortest path length = "
+         << sol.shortestPathBinaryMatrix(grid) << endl;
+    // Expected output: 2
+
+    return 0;
+}
