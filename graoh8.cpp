4ea4dc8e03a8d3930ecedebcc095989e2f498983
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 16:36:10 +0530

    New commit

--- graoh8.cpp
+++ graoh8.cpp
@@ -1,17 +1,23 @@
-int DFS(unordered_map<int, vector<int>> &adj, int curr, int parent, vector<bool>& hasApple) {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int DFS(unordered_map<int, vector<int>> &adj, int curr, int parent, vector<bool>& hasApple) {
         int time = 0;
         
-        for(int &child : adj[curr]) {
-            if(child == parent)
+        for (int child : adj[curr]) {
+            if (child == parent)
                 continue;
             
-            int time_from_bachha_log = DFS(adj, child, curr, hasApple);
+            int childTime = DFS(adj, child, curr, hasApple);
             
-            if(time_from_bachha_log || hasApple[child])
-                time += 2 + time_from_bachha_log;
-            
+            // If child subtree has apples or the child itself has an apple,
+            // we need to go to that child and come back => +2 edges
+            if (childTime > 0 || hasApple[child]) {
+                time += 2 + childTime;
+            }
         }
-        
         
         return time;
     }
@@ -19,12 +25,29 @@ int DFS(unordered_map<int, vector<int>> &adj, int curr, int parent, vector<bool>
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
         unordered_map<int, vector<int>> adj;
         
-        for(auto &vec : edges) {
+        for (auto &vec : edges) {
             int u = vec[0];
             int v = vec[1];
-            
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         
         return DFS(adj, 0, -1, hasApple);
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int n = 7;
+    vector<vector<int>> edges = {
+        {0,1}, {0,2}, {1,4}, {1,5}, {2,3}, {2,6}
+    };
+    vector<bool> hasApple = {false, false, true, false, true, true, false};
+
+    int ans = sol.minTime(n, edges, hasApple);
+    cout << "Minimum time to collect all apples: " << ans << endl;
+    // Expected output: 8
+
+    return 0;
+}
