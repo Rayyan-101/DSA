87eddd48faa0dee70fe4eb16e0e4e8330e35cb1e
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-11-29 19:19:41 +0530

    New commit

--- graph24.cpp
+++ graph24.cpp
@@ -1,10 +1,16 @@
-typedef pair<double, int> P;
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<double, int> P;  // {probability, node}
+
+class Solution {
+public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
         unordered_map<int, vector<pair<int, double>>> adj;
-        vector<double> result(n, 0);
-        
-        for(int i=0;i<edges.size();i++)
-        {
+        vector<double> result(n, 0.0);
+
+        // Build adjacency list
+        for (int i = 0; i < (int)edges.size(); i++) {
             int u       = edges[i][0];
             int v       = edges[i][1];
             double prob = succProb[i];
@@ -12,26 +18,51 @@ typedef pair<double, int> P;
             adj[v].push_back({u, prob});
         }
 
-        priority_queue<P> pq; //max heap because we need maximum probability
+        // Max-heap based on probability
+        priority_queue<P> pq; // {prob, node}
+        result[start] = 1.0;
+        pq.push({1.0, start});
 
-		result[start] = 1;
-		pq.push({1.0, start});
-
-		while(!pq.empty()) {
-
-		    int curnode    = pq.top().second;
+        while (!pq.empty()) {
             double curProb = pq.top().first;
+            int curnode    = pq.top().second;
             pq.pop();
 
-            for(auto &child : adj[curnode])
-            {
-                if(result[child.first] < curProb*child.second) //we want maximum this time
-                {
-                    result[child.first] = curProb*child.second;
-                    pq.push({result[child.first], child.first});
+            // Optional small optimization: if this is already worse than stored, skip
+            if (curProb < result[curnode]) continue;
+
+            for (auto &child : adj[curnode]) {
+                int nextNode   = child.first;
+                double edgeProb = child.second;
+
+                double newProb = curProb * edgeProb;
+                if (result[nextNode] < newProb) {  // we want maximum probability
+                    result[nextNode] = newProb;
+                    pq.push({newProb, nextNode});
                 }
             }
+        }
 
-		}
+        return result[end];
+    }
+};
 
-		return result[end];
+int main() {
+    Solution sol;
+
+    int n = 3;
+    vector<vector<int>> edges = {
+        {0, 1},
+        {1, 2},
+        {0, 2}
+    };
+    vector<double> succProb = {0.5, 0.5, 0.2};
+    int start = 0, end = 2;
+
+    double ans = sol.maxProbability(n, edges, succProb, start, end);
+    cout << fixed << setprecision(5);
+    cout << "Maximum probability from " << start << " to " << end << " = " << ans << endl;
+    // Expected: 0.25 (best path 0 -> 1 -> 2 with prob 0.5 * 0.5)
+
+    return 0;
+}
