26fe955e12b69007a567167bc502dc5991ee2d2d
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-01 00:33:46 +0530

    New commit

--- graph29.cpp
+++ graph29.cpp
@@ -1,54 +1,80 @@
-typedef pair<int, int> P;
-    
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> P; // {node, mask}
+
+class Solution {
+public:
     int shortestPathLength(vector<vector<int>>& graph) {
         int n = graph.size();
-        
-        if(n == 1 || n == 0)
-            return 0;
-        
-        queue<P> que;
-        
-        set<P> vis;
-        //Instead of set, you can also use : vector<vector<bool>> visited(n, vector<bool>(finalState, false));
-        //Just like done in Java code below
+        if (n <= 1) return 0;
 
-        int allVisitedState = (1 << n) - 1; // if all nodes visited then
-            
-        for(int i = 0; i < n; i++) {
+        // BFS queue: (current node, bitmask of visited nodes)
+        queue<P> que;
+
+        // Visited states: (node, mask)
+        set<P> vis;
+        // Alternative faster option:
+        // int maxMask = 1 << n;
+        // vector<vector<bool>> vis(n, vector<bool>(maxMask, false));
+
+        int allVisitedState = (1 << n) - 1; // all bits set => all nodes visited
+
+        // Multi-source BFS: start from every node
+        for (int i = 0; i < n; i++) {
             int maskValue = (1 << i);
-            
             que.push({i, maskValue});
-            
             vis.insert({i, maskValue});
+            // vis[i][maskValue] = true; // if using 2D visited array
         }
-        
+
         int result = 0;
-        
-        // Implementing BFS
-        while(!que.empty())  {
-            
-            int N = que.size();
+
+        // BFS
+        while (!que.empty()) {
+            int sz = que.size();
             result++;
-            while(N--) {
-                
+
+            while (sz--) {
                 auto curr = que.front();
                 que.pop();
-                
+
                 int currNode = curr.first;
                 int currMask = curr.second;
-                
-                for(auto &adj: graph[currNode]) {
-                    int nextMask = currMask | (1 << adj);
-                    
-                    if(nextMask == allVisitedState)
+
+                for (int adjNode : graph[currNode]) {
+                    int nextMask = currMask | (1 << adjNode);
+
+                    if (nextMask == allVisitedState) {
                         return result;
-                    
-                    if(vis.find({adj, nextMask}) == vis.end()) {
-                        vis.insert({adj, nextMask});
-                        que.push({adj, nextMask});                      
                     }
-                }  
+
+                    P state = {adjNode, nextMask};
+                    if (vis.find(state) == vis.end()) {
+                        vis.insert(state);
+                        que.push(state);
+                        // if (!vis[adjNode][nextMask]) { vis[adjNode][nextMask] = true; que.push({adjNode, nextMask}); }
+                    }
+                }
             }
         }
 
-        return -1;
+        return -1; // shouldn't happen for connected graphs
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> graph = {
+        {1,2,3},  // 0
+        {0},      // 1
+        {0},      // 2
+        {0}       // 3
+    };
+
+    cout << "Shortest path length visiting all nodes: "
+         << sol.shortestPathLength(graph) << endl;  // Expected: 4
+
+    return 0;
+}
