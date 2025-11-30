f104d7884d323d2b3ffabbdb0835d401ad2900af
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-01 00:25:10 +0530

    New commit

--- graph27.cpp
+++ graph27.cpp
@@ -1,8 +1,13 @@
-int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<int> degree(n);
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<int> degree(n, 0);
         vector<vector<bool>> connected(n, vector<bool>(n, false));
         
-        for(auto &road : roads) {
+        for (auto &road : roads) {
             int u = road[0];
             int v = road[1];
             
@@ -15,21 +20,36 @@ int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         
         int maxRank = 0;
         
-        for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int rank = degree[i] + degree[j];
                 
-                int i_rank = degree[i];
-                int j_rank = degree[j];
-                
-                int rank   = i_rank + j_rank;
-                
-                if(connected[i][j] || connected[j][i]) {
+                // if there is a direct road between i and j, subtract 1
+                if (connected[i][j]) {
                     rank -= 1;
                 }
                 
                 maxRank = max(maxRank, rank);
-                
             }
         }
         
         return maxRank;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int n = 4;
+    vector<vector<int>> roads = {
+        {0,1},
+        {0,3},
+        {1,2},
+        {1,3}
+    };
+
+    int ans = sol.maximalNetworkRank(n, roads);
+    cout << "Maximal network rank: " << ans << endl;  // Expected: 4
+
+    return 0;
+}
