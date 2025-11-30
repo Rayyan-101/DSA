03e999f09be3b2af8407eb291cbecaeeebe161df
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-01 00:28:12 +0530

    New commit

--- graph28.cpp
+++ graph28.cpp
@@ -1,55 +1,80 @@
-unordered_map<string, vector<string>> adj;
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    unordered_map<string, vector<string>> adj;
     vector<string> route;
     int numTickets = 0;
-    
-    vector<string> findItinerary(vector<vector<string>>& tickets) {
-		numTickets = tickets.size();
+
+    bool DFS(const string &fromAirport, vector<string>& temp) {
+        temp.push_back(fromAirport);
         
-		for (auto& t : tickets) {
+        // If we have used all tickets (numTickets edges), path length = numTickets + 1
+        if ((int)temp.size() == numTickets + 1) {
+            route = temp;
+            return true;
+        }
+        
+        vector<string>& neighbors = adj[fromAirport];
+        
+        // Try neighbors in lexical order (already sorted)
+        for (int i = 0; i < (int)neighbors.size(); i++) {
+            string toAirport = neighbors[i];
+
+            // Remove this ticket so we don't reuse it
+            neighbors.erase(neighbors.begin() + i);
+
+            if (DFS(toAirport, temp))
+                return true;
+
+            // Backtrack: restore ticket
+            neighbors.insert(neighbors.begin() + i, toAirport);
+        }
+
+        temp.pop_back();
+        return false;
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        adj.clear();
+        route.clear();
+
+        numTickets = (int)tickets.size();
+        
+        for (auto &t : tickets) {
             string u = t[0];
             string v = t[1];
-            
-			adj[u].push_back(v);
-		}
+            adj[u].push_back(v);
+        }
         
-        // Sort vertices in the adjacency list by lexical order
+        // Sort adjacency lists to ensure lexicographically smallest route
         for (auto &edges : adj) {
-            sort(begin(edges.second), end(edges.second));
+            sort(edges.second.begin(), edges.second.end());
         }
         
         vector<string> temp;
         DFS("JFK", temp);
         return route;
     }
-    
-    bool DFS(string fromAirport, vector<string>& temp) {
-        temp.push_back(fromAirport);
-        
-        // we have used all tickets, this is a valid path return the result
-        if (temp.size() == numTickets+1) {
-            route = temp;
-            return true;
-        }
-        
-        //You have already found the route (first valid lexicographically path)
+};
 
-        
-        vector<string>& neighbors = adj[fromAirport];
-        
-        // at the current layer, try its neighbors in the sorted order
-        for(int i = 0; i < neighbors.size(); i++) {
-            
-            string toAirport = neighbors[i];
-            
-            // remove ticket(route) from graph so that it won't be reused 
-            neighbors.erase(neighbors.begin()+i);
-            
-            if(DFS(toAirport, temp))
-                return true;
-            
-            // if the current path is invalid, restore the current ticket
-            neighbors.insert(neighbors.begin()+i, toAirport);            
-        }
-        
-        temp.pop_back();
-        return false;
+int main() {
+    Solution sol;
+
+    vector<vector<string>> tickets = {
+        {"MUC","LHR"},
+        {"JFK","MUC"},
+        {"SFO","SJC"},
+        {"LHR","SFO"}
+    };
+
+    vector<string> ans = sol.findItinerary(tickets);
+
+    cout << "Itinerary: ";
+    for (auto &s : ans) cout << s << " ";
+    cout << endl;
+    // Expected: JFK MUC LHR SFO SJC
+
+    return 0;
+}
