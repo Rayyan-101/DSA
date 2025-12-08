d2b7dcd3ac001323c588b0fe30b653cf9ef36b10
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-08 16:13:00 +0530

    New commit

--- graph34.cpp
+++ graph34.cpp
@@ -1,35 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 vector<int> countOfPairs(int n, int x, int y) {
-        vector<vector<int>> grid(n+1, vector<int>(n+1, 100000));
-        
-        for(int j = 1; j < n; j++) {
-            grid[j][j+1] = 1;
-            grid[j+1][j] = 1;
-        }
-        grid[x][y] = 1;
-        grid[y][x] = 1;
-        
-        for(int via = 1; via <= n; via++) {
-            
-            for(int i = 1; i <= n; i++) {
-                for(int j = 1; j <= n; j++) {
-                    
-                   grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
-                    
-                }
-            }
-        }
-        
-        vector<int> result(n);
+
+    vector<vector<int>> grid(n+1, vector<int>(n+1, 100000));
+
+    // Adjacent edges
+    for(int j = 1; j < n; j++) {
+        grid[j][j+1] = 1;
+        grid[j+1][j] = 1;
+    }
+
+    // Special extra edge
+    grid[x][y] = 1;
+    grid[y][x] = 1;
+
+    // Floyd–Warshall
+    for(int via = 1; via <= n; via++) {
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= n; j++) {
-
-                if(i != j) {
-                    int val = grid[i][j];
-                    result[val-1]++;
-                }
-
+                grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
             }
         }
-        
-        return result;
-        
+    }
+
+    // Count distances
+    vector<int> result(n);
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(i != j) {
+                int d = grid[i][j];
+                result[d - 1]++;
+            }
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int n, x, y;
+    cout << "Enter n, x, y: ";
+    cin >> n >> x >> y;
+
+    vector<int> ans = countOfPairs(n, x, y);
+
+    cout << "Result: ";
+    for(int v : ans) cout << v << " ";
+    cout << endl;
+
+    return 0;
+}
