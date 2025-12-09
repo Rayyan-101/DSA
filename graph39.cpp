9107642b8f2ccb7c7e14547374dc3c2986c9b67a
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-10 00:08:30 +0530

    New commit

--- graph39.cpp
+++ graph39.cpp
@@ -1,25 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 long long maximumImportance(int n, vector<vector<int>>& roads) {
-        //n = total number of nodes numbered from 0 to n-1
+    // n = number of cities (0 to n-1)
 
-        vector<int> degree(n, 0);
+    vector<int> degree(n, 0);
 
-        for(auto &vec : roads) {
-            int u = vec[0];
-            int v = vec[1];
-
-            degree[u]++;
-            degree[v]++;
-        }
-
-        sort(begin(degree), end(degree)); //ascedning order
-
-        long long value = 1;
-        long long sum   = 0;
-
-        for(int i = 0; i < n; i++) {
-            sum += (degree[i] * value);
-            value++;
-        }
-
-        return sum;
+    for (auto &vec : roads) {
+        int u = vec[0];
+        int v = vec[1];
+        degree[u]++;
+        degree[v]++;
     }
+
+    // Sort degrees in ascending order
+    sort(begin(degree), end(degree));
+
+    long long value = 1;
+    long long sum   = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += (long long)degree[i] * value;
+        value++;
+    }
+
+    return sum;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> roads(m, vector<int>(2));
+
+    for (int i = 0; i < m; i++) {
+        cin >> roads[i][0] >> roads[i][1];
+    }
+
+    cout << maximumImportance(n, roads) << "\n";
+
+    return 0;
+}
