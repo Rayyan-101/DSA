8cb2d94fa0395b89ef26efa1ac7274aa282bd9e7
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-11 00:01:07 +0530

    New commit

--- graph41.cpp
+++ graph41.cpp
@@ -1,68 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int node, unordered_map<int, vector<int>>& adj, vector<int>& visited,
+         stack<int>& st, bool& hasCycle) {
+    
+    visited[node] = 1; // visiting
+
+    for (int& nbr : adj[node]) {
+        if (visited[nbr] == 0) {
+            dfs(nbr, adj, visited, st, hasCycle);
+            if (hasCycle) return;
+        } 
+        else if (visited[nbr] == 1) {
+            hasCycle = true;
+            return;
+        }
+    }
+
+    visited[node] = 2; // visited
+    st.push(node);
+}
+
 vector<int> topoSort(vector<vector<int>>& edges, int n) {
-        unordered_map<int, vector<int>> adj;
-        stack<int> st;
-        vector<int> order;
-        //0 : not visited;
-        //1 : visiting (currently in stack via some other DFS)
-        //2 : visited
-        vector<int> visited(n + 1, 0);
-        bool hasCycle = false;
+    unordered_map<int, vector<int>> adj;
+    vector<int> visited(n + 1, 0);
+    bool hasCycle = false;
+    stack<int> st;
 
-        for (vector<int>& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            adj[u].push_back(v);
-        }
-
-        for (int i = 1; i <= n; i++) {
-            if (visited[i] == 0) {
-                dfs(i, adj, visited, st, hasCycle);
-                if (hasCycle) 
-                    return {}; //no ordering possible
-            }
-        }
-        
-        while(!st.empty()) {
-            order.push_back(st.top());
-            st.pop();
-        }
-        return order;
+    for (auto& e : edges) {
+        int u = e[0];
+        int v = e[1];
+        adj[u].push_back(v);
     }
 
-    void dfs(int node, unordered_map<int, vector<int>>& adj, vector<int>& visited,
-             stack<int>& st, bool& hasCycle) {
-        
-        visited[node] = 1;  // Mark node as visiting
-        //First, visit node's children
-        for (int& nbr : adj[node]) {
-            if (visited[nbr] == 0) {
-                dfs(nbr, adj, visited, st, hasCycle);
-            } else if (visited[nbr] == 1) {
-                // Cycle detected
-                hasCycle = true;
-                return;
-            }
+    for (int i = 1; i <= n; i++) {
+        if (visited[i] == 0) {
+            dfs(i, adj, visited, st, hasCycle);
+            if (hasCycle) return {};
         }
-
-        visited[node] = 2; //visited
-        st.push(node); //Now node can be added
     }
 
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions,
-                                    vector<vector<int>>& colConditions) {        
-        vector<int> orderRows    = topoSort(rowConditions, k);
-        vector<int> orderColumns = topoSort(colConditions, k);
+    vector<int> order;
+    while (!st.empty()) {
+        order.push_back(st.top());
+        st.pop();
+    }
 
-        // We might have found cycle. That's why topo order is empty
-        if (orderRows.empty() or orderColumns.empty())
-            return {};
+    return order;
+}
 
-        vector<vector<int>> matrix(k, vector<int>(k, 0));
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < k; j++) {
-                if (orderRows[i] == orderColumns[j]) {
-                    matrix[i][j] = orderRows[i];
-                }
-            }
-        }
-        return matrix;
+vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions,
+                                vector<vector<int>>& colConditions) {
+    
+    vector<int> orderRows = topoSort(rowConditions, k);
+    vector<int> orderCols = topoSort(colConditions, k);
+
+    if (orderRows.empty() || orderCols.empty())
+        return {};
+
+    vector<vector<int>> matrix(k, vector<int>(k, 0));
+
+    unordered_map<int, int> rowPos, colPos;
+
+    // Store row positions
+    for (int i = 0; i < k; i++) rowPos[orderRows[i]] = i;
+    
+    // Store col positions
+    for (int i = 0; i < k; i++) colPos[orderCols[i]] = i;
+
+    // Place each number 1..k at its (row, col)
+    for (int num = 1; num <= k; num++) {
+        int r = rowPos[num];
+        int c = colPos[num];
+        matrix[r][c] = num;
+    }
+
+    return matrix;
+}
+
+// ---------------------------
+//            MAIN
+// ---------------------------
+
+int main() {
+    int k, r, c;
+    cout << "Enter k (#elements), #rowConditions, #colConditions: ";
+    cin >> k >> r >> c;
+
+    vector<vector<int>> rowConditions(r, vector<int>(2));
+    vector<vector<int>> colConditions(c, vector<int>(2));
+
+    cout << "Enter row conditions (u v meaning u before v):\n";
+    for (int i = 0; i < r; i++)
+        cin >> rowConditions[i][0] >> rowConditions[i][1];
+
+    cout << "Enter column conditions (u v meaning u before v):\n";
+    for (int i = 0; i < c; i++)
+        cin >> colConditions[i][0] >> colConditions[i][1];
+
+    vector<vector<int>> result = buildMatrix(k, rowConditions, colConditions);
+
+    if (result.empty()) {
+        cout << "No valid matrix exists (cycle detected)\n";
+        return 0;
+    }
+
+    cout << "\nConstructed Matrix:\n";
+    for (auto& row : result) {
+        for (int x : row)
+            cout << x << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
