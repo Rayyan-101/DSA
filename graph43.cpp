c7db78e5d92cacace45501aa494475abe11329ae
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-11 00:05:48 +0530

    New commit

--- graph43.cpp
+++ graph43.cpp
@@ -1,23 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 int findChampion(int n, vector<vector<int>>& edges) {
-        vector<int> indegree(n, 0);
-        for(vector<int>& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
+    vector<int> indegree(n, 0);
 
-            //u --> v
-            indegree[v]++;
+    for (vector<int>& edge : edges) {
+        int u = edge[0];
+        int v = edge[1];
+
+        // u → v
+        indegree[v]++;
+    }
+
+    int champ = -1;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (indegree[i] == 0) {
+            champ = i;
+            count++;
+            if (count > 1) return -1; // more than one possible champion
         }
+    }
 
-        int champ = -1;
-        int count = 0;
-        for(int i = 0; i < n; i++) {
-            if(indegree[i] == 0) { //ith node is the champion
-                champ = i;
-                count++;
-                if(count > 1) {
-                    return -1;
-                }
-            }
-        }
+    return champ;
+}
 
-        return champ;
+int main() {
+    int n, m;
+    cout << "Enter number of nodes (n) and edges (m): ";
+    cin >> n >> m;
+
+    vector<vector<int>> edges(m, vector<int>(2));
+    cout << "Enter directed edges (u v meaning u -> v):\n";
+    for (int i = 0; i < m; i++) {
+        cin >> edges[i][0] >> edges[i][1];
+    }
+
+    int champion = findChampion(n, edges);
+
+    if (champion == -1)
+        cout << "No unique champion exists.\n";
+    else
+        cout << "Champion node: " << champion << "\n";
+
+    return 0;
+}
