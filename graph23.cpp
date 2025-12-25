e31e32008f28832a64fd2091b1509f04916ebd6d
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-25 15:43:09 +0530

    New commit

--- graph23.cpp
+++ graph23.cpp
@@ -24,7 +24,7 @@ public:
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 
-                if (i == j) 
+                if (i == j) // same bomb
                     continue;
                 
                 LL x1 = bombs[i][0];
