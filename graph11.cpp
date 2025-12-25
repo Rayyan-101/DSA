bf135c8e9a191a54ffa9bc68c2727f0d716278cb
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-25 15:44:08 +0530

    New commit

--- graph11.cpp
+++ graph11.cpp
@@ -36,7 +36,7 @@ public:
         
         for (int i = 0; i < n; i++) {
             if (dist1[i] == INT_MAX || dist2[i] == INT_MAX)
-                continue;  // unreachable, skip
+                continue;  
             
             int maxD = max(dist1[i], dist2[i]);
             
