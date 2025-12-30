8ae8d0ca8e6575368315c60f1a7d791f0a50282b
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-30 10:41:43 +0530

    New commit

--- graph20.cpp
+++ graph20.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class DSU {
 private:
     vector<int> parent;
-    vector<int> rankv;   
+    vector<int> rankv;   // renamed from rank to avoid name clashes
     int components;
 
 public: 
