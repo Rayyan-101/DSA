55e7bf3244ae435e26d1558e4050d04500d13803
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-30 10:41:15 +0530

    New commit

--- graph20.cpp
+++ graph20.cpp
@@ -20,7 +20,7 @@ public:
     int find(int x) {
         if (x == parent[x])
             return x;
-        return parent[x] = find(parent[x]); // path compression
+        return parent[x] = find(parent[x]); 
     }
 
     void Union(int x, int y) {
