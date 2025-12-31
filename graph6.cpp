4bb13cea0c68a417c5ea274d3e8ba9150e70c106
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2025-12-31 16:14:19 +0530

    New commit

--- graph6.cpp
+++ graph6.cpp
@@ -17,7 +17,7 @@ public:
                 if (color[v] == color[u])
                     return false;  // same color on both ends -> not bipartite
                 
-                if (color[v] == -1) { // not colored yet
+                if (color[v] == -1) { 
                     color[v] = 1 - color[u];
                     que.push(v);
                 }
