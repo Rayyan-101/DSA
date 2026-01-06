8de2161260b76d9e5463510fb034b5138cf6aabd
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2026-01-06 14:36:21 +0530

    New Commit

--- graph23.cpp
+++ graph23.cpp
@@ -20,7 +20,6 @@ public:
         
         unordered_map<int, vector<int>> adj;
         
-        // Build directed graph: edge i -> j if bomb i can detonate bomb j
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 
