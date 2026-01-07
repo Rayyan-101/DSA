107dd51f6e0487237eeb10b9b58cf4c4232bfd65
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2026-01-07 08:43:01 +0530

    New Commit

--- graph15.cpp
+++ graph15.cpp
@@ -34,7 +34,6 @@ public:
                 dfs(i, adj, visited, sizeOfComponent);
                 
                 // pairs between this component and all remaining nodes outside it
-                result += sizeOfComponent * (remainingNodes - sizeOfComponent);
                 remainingNodes -= sizeOfComponent;
             }
         }
