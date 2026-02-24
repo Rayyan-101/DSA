ceadfba7bc2b63d5ee9a4357db0fd714e6a7e95a
Lulaniya Mohd Rayyan <rayyanlulaniya1990@gmail.com>
2026-02-24 14:43:08 +0530

    Update and rename array to array1.cpp

---
+++ array1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+  
+    bool isPalindrome(int arr){
+        string s=to_string(arr);
+        int start=0;
+        int end=s.size()-1;
+        
+        bool flag=true;
+        
+        while(start<end){
+            if(s[start]!=s[end]){
+                flag=false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        return flag;
+    }
+  
+    bool isPalinArray(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(!isPalindrome(arr[i])){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
