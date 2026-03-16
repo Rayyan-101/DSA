7f3567589b405218f2f1bf89c096695dbe550a91
Lulaniya Mohd Rayyan <rayyanlulaniya1990@gmail.com>
2026-03-16 14:55:52 +0530

    Create trees2.cpp

---
+++ trees2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int dfs(Node *root, int sum, int k, map<int,int>&mp)
+    {
+        if(!root) return 0;
+        sum += root->data;
+        int res = mp[sum - k];
+        
+        mp[sum]++;
+        if(root->left) res += dfs(root->left, sum, k, mp);
+        if(root->right) res += dfs(root->right, sum, k, mp);
+        mp[sum]--;
+        return res;
+    }
+  public:
+    int countAllPaths(Node *root, int k) {
+        // code here
+        map<int, int>mp;
+        mp[0] = 1;
+        
+        int sum = 0;
+        return dfs(root, sum, k, mp);
+    }
+};
