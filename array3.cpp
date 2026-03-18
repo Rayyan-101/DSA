153d8ba8f07940b3206d5a19eeeb6d5a4986728b
Lulaniya Mohd Rayyan <rayyanlulaniya1990@gmail.com>
2026-03-18 19:24:42 +0530

    Update array3.cpp

--- array3.cpp
+++ array3.cpp
@@ -1,7 +1,6 @@
 class Solution {
   public:
     void pushZerosToEnd(vector<int>& arr) {
-        // code here
         int cnt = 0;
         for(int i = 0;i<arr.size();i++){
             if(arr[i] == 0)cnt++;
