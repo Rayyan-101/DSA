606b54ce7dda4b6c7614d0b27b5311f8a2b60f9e
Lulaniya Mohd Rayyan <rayyanlulaniya1990@gmail.com>
2026-03-18 19:23:35 +0530

    Update trees4.cpp

--- trees4.cpp
+++ trees4.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+  public:
+    pair<int,int> solve(Node* root){
+        if(!root)return {0,0};
+        pair<int,int> l = solve(root->left);
+        pair<int,int> r = solve(root->right);
+        
+        int cnt = root->data + l.first + r.first - 1;
+        int cost = abs(cnt) + l.second + r.second;
+        
+        return {cnt, cost};
+    }
+    int distCandy(Node* root) {
+        pair<int,int>  p = solve(root);
+        return p.second;
+        
+    }
+};
