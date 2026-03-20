1e85eaff9e9a1e4a0ff2bcc29f4a3ba3fdff6c5d
Lulaniya Mohd Rayyan <rayyanlulaniya1990@gmail.com>
2026-03-20 11:11:19 +0530

    Create trees6.cpp

---
+++ trees6.cpp
@@ -0,0 +1,28 @@
+  public:
+  
+    void solve(Node* root,int key,Node* &small,Node* & greater){
+      if(root == NULL){
+          return;
+      }
+      
+      if(root->left)solve(root->left,key,small,greater);
+      
+      if(root->data < key and (small == NULL or small->data < root->data)){
+            small = root;
+          }
+          
+      if(root->data > key and (greater == NULL or greater->data > root->data)){
+              greater = root;
+          }
+          
+      if(root->right)solve(root->right,key,small,greater);
+      }
+      
+    vector<Node*> findPreSuc(Node* root, int key) {
+        // code here
+        Node* small = NULL;
+        Node* greater = NULL;
+        solve(root,key,small,greater);
+        return {small,greater};
+    }
+};
