5e3fbbe444bf50fb75f3d1a51f83ff59e8542576
Lulaniya Mohd Rayyan <rayyanlulaniya1990@gmail.com>
2026-03-28 12:09:48 +0530

    Update strings6.cpp

--- strings6.cpp
+++ strings6.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
         
-    int n=nums.size();
-
+        int n=nums.size();
         string result;
         
         for(int i=0;i<n;i++){
