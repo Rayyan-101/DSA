9a817cbd7a12b72d515a5bc1a23e57c6ca6a8e48
Lulaniya Mohammad Rayyan <rayyanlulaniya1990@gmail.com>
2026-04-08 09:53:45 +0530

    Update dp1.cpp

--- dp1.cpp
+++ dp1.cpp
@@ -3,6 +3,7 @@ public:
     int maximumAmount(vector<vector<int>>& coins) {
          int n = coins.size(), m = coins[0].size();
         vector dp(n, vector(m, vector<int>(3, -1e9)));
+        
         dp[0][0][1] = dp[0][0][2] = 0, dp[0][0][0] = coins[0][0];
         
         for (int i = 0; i < n; i++)
