0289c54d03b725de53a33ce4bd24e84f20d802d3
Lulaniya Mohammad Rayyan <rayyanlulaniya1990@gmail.com>
2026-05-01 16:46:19 +0530

    Create Heap1.cpp

---
+++ Heap1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> kthLargest(vector<int>& arr, int k) {
+        // code here
+        priority_queue<int> pq;
+        vector<int> result;
+        for(int i = 0;i<k-1;i++)
+        {
+            pq.push(-arr[i]);
+            result.push_back(-1);
+        }
+        
+        for(int i = k-1;i<arr.size();i++)
+        {
+            pq.push(-arr[i]);
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+            result.push_back(-1*pq.top());
+        }
+        return result;
+    }
+};
