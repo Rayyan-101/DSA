46f1db2270bc35bee79e00dc57adfc23b1708968
Lulaniya Mohammad Rayyan <rayyanlulaniya1990@gmail.com>
2026-05-03 06:40:57 +0530

    Create array15.cpp

---
+++ array15.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> sortBySetBitCount(vector<int>& arr) {
+        // code here
+        vector<pair<int,int>> v;
+        vector<int> ans ;
+        int n = arr.size() ;
+        for(int i=0;i<n;i++){
+            int ff = __builtin_popcount(arr[i]);
+            v.push_back({ff,-i});
+        }
+        
+        sort(v.rbegin(),v.rend()) ;
+        for(auto [a,b] : v) ans.push_back(arr[-b]);
+        
+        return ans ;
+    }
+};
