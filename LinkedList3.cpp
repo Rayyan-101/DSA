a15f6d3b1df891acbeab08ab0e0f0bdea1255ca2
Lulaniya Mohammad Rayyan <rayyanlulaniya1990@gmail.com>
2026-06-15 11:35:28 +0530

    Create LinkedList3.cpp

---
+++ LinkedList3.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void delet(ListNode* &head,ListNode* h)
+    {
+        if(h==nullptr || h->next==nullptr){
+           head=head->next;
+        }
+        else{
+            delet(head->next,h->next->next);
+        }
+    }
+    ListNode* deleteMiddle(ListNode* head) {
+        delet(head,head);
+        return head;
+    }
+};
