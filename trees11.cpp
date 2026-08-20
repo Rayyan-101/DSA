d5ac9dce87742eba29492ab820acfa1ad6ffe716
Lulaniya Mohammad Rayyan <rayyanlulaniya1990@gmail.com>
2026-08-20 15:34:05 +0530

    Create trees11.cpp

---
+++ trees11.cpp
@@ -0,0 +1,36 @@
+/* Structure of Binary Tree Node
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+      int maxi = INT_MIN;
+
+      void solve(Node* root, int mx) {
+          if (!root) return;
+
+          maxi = max(maxi, mx - root->data);
+
+          mx = max(mx, root->data);
+
+          solve(root->left, mx);
+          solve(root->right, mx);
+      }
+
+      int maxDiff(Node* root) {
+          if (!root) return 0;
+
+          solve(root->left, root->data);
+          solve(root->right, root->data);
+
+          return maxi;
+      }
+  };
